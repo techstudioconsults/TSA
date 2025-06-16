428649863ba2a7265e8ddf070b864323dd5dfd26
Kingsley Ifijeh <kinxly@gmail.com>
2025-06-16 06:29:42 +0100

    patch: added script for facebook-tracker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import React, { Suspense } from "react";
 
 import "./setup.ts";
+
+import Image from "next/image.js";
+import Script from "next/script.js";
 
 import { WhatsAppIcon } from "~/components/miscellaneous/whatsapp";
 import { Toast } from "~/components/Toast";
@@ -19,6 +24,32 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Facebook Pixel Script */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '962870014990453');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <Image
+            alt={`facebook-img`}
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=962870014990453&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body className={cn(openSans.className)}>
         <Toast />
         <WhatsAppIcon />
